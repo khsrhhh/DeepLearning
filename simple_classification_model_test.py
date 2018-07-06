b1fb4f1df90cd4b4d7baac753a420c0e75eb1a29
Choi Daeho <khsrhhh@gmail.com>
2018-07-06 20:03:09 +0900

    set 2 layers

--- simple_classification_model_test.py
+++ simple_classification_model_test.py
@@ -27,19 +27,25 @@ y_data = np.array([
     [0,1,0]
 ])
 
-#number of classes
+#number of classes, neuron
 n_classes = 3
+n_neuron = 10
 
 X = tf.placeholder("float", [None, 2])
 Y = tf.placeholder("float", [None, 3])
 
-W = tf.Variable(tf.random_normal([2, n_classes]), name = 'weight')
-b = tf.Variable(tf.random_normal([n_classes]), name = 'bias')
+#[2, 10] -> [input, neuron]
+W1 = tf.Variable(tf.random_normal([2, n_neuron]), name = 'weight1')
+b1 = tf.Variable(tf.random_normal([n_neuron]), name = 'bias1')
+W2 = tf.Variable(tf.random_normal([n_neuron, n_classes]), name = 'weight2')
+b2 = tf.Variable(tf.random_normal([n_classes]), name = 'bias2')
 
-L1 = tf.add(tf.matmul(X, W), b)
+#set two layers
+L1 = tf.add(tf.matmul(X, W1), b1)
 L1 = tf.nn.relu(L1)
+L2 = tf.add(tf.matmul(L1, W2), b2)
 
-model = tf.nn.softmax(L1)
+model = tf.nn.softmax(L2)
 
 #Cross entropy cost/Loss
 cost = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(model), axis = 1))
@@ -71,5 +77,6 @@ accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 print('accuracy: %.2f' % sess.run(accuracy * 100, feed_dict = {X: x_data, Y: y_data}))
 
 '''
-poor accuracy
+between 50 to 100 accuracy
+
 '''
