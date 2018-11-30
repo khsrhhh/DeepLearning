5d3df58afdbe905d7f2a3ebb29fd2febff9553ec
khsrhhh <39553283+khsrhhh@users.noreply.github.com>
2018-11-30 17:59:50 +0900

    add line about website

---
+++ basic_classification_using_keras.py
@@ -0,0 +1,9 @@
+#https://www.tensorflow.org/tutorials/keras/basic_classification
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
